f72ab841bf489119386a1bd7daad6d3c6cc96c2e
francesca <francescapellecchia96@gmail.com>
2019-04-08 13:17:16 +0200

    update

--- Insert.js
+++ Insert.js
@@ -2,62 +2,63 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require("body-parser");
 var app = express();
-var a_t = '';
+var server = require('./app');
+var ws=require('ws');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/login', function(req, res){
-  res.redirect("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/calendar&response_type=code&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http%3A%2F%2Flocalhost:3000&client_id=client_id");
-});
+function accessoGoogle(req, res){
+  res.redirect("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/calendar&response_type=code&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http%3A%2F%2Flocalhost:8888&client_id=client-id");
+};
 
-app.get('/', function(req, res){
-  console.log("code taken");
-  
-
-  var formData = {
-    code: req.query.code,
-    client_id: 'clien_id',
-    client_secret: 'client_secret',
-    redirect_uri: 'http://localhost:3000',
-    grant_type: 'authorization_code',
-		
-  }
-
-
-  request.post({url:'https://www.googleapis.com/oauth2/v4/token', form: formData}, function optionalCallback(err, httpResponse, body) {
-  if (err) {
-    return console.error('upload failed:', err);
-  } 
-  console.log('Upload successful!  Server responded with:', body);
-  var info = JSON.parse(body);
-  res.send("Got the token "+ info.access_token);
-  a_t = info.access_token;a_t = info.access_token;
-});
-
-});
-
-
-app.get('/insert', function(req, res){
+function aggiungiEvento(req,res,a_t){
   var url= 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
-  
-var headers= {
+  var info= server.getEvento();
+	var cantante=server.getCantante();
+	var lista=info.split(" ");
+	var luogo=lista[1];
+	var citta=server.getCitta();
+	var completa=lista[0];
+	if(completa.length>11){
+		var data=completa.split("T")[0];
+		var completa2=completa.split("+")[0];
+		var headers= {
     'Authorization': 'Bearer '+a_t,
 		'Content-Type': 'application/json'
-		
+
     };
-	var body1={
-	'descrition' : 'Evento musicale',
-	'summary' : 'Concerto',
-	'location': 'Palalottomatica(Roma)',
-	'end': {
-	'dateTime' : '2019-03-09T23:00:00+01:00',
-	'timeZone': 'Europe/Rome'
-	},
-	'start':{
-	'dateTime': '2019-03-09T21:00:00+01:00',
-	'timeZone' : 'Europe/Rome'
+		var body1={
+		'descrition' : 'Evento musicale',
+		'summary' : 'Concerto di '+cantante,
+		'location': luogo,
+		'end': {
+		'dateTime' : data+'T23:59:00+02:00',
+		'timeZone': 'Europe/'+citta
+		},
+		'start':{
+		'dateTime': completa2+'+02:00',
+		'timeZone' : 'Europe/'+citta
+		}
+		};
+	}else{
+		console.log(completa);
+		var headers= {
+				'Authorization': 'Bearer '+a_t,
+				'Content-Type': 'application/json'
+
+				};
+			var body1={
+			'descrition' : 'Evento musicale',
+			'summary' : 'Concerto di '+cantante,
+			'location': luogo,
+			'end': {
+			'date' : completa
+			},
+			'start':{
+			'date': completa
+			}
+			};
 	}
-  };
-	
+
   request.post({headers: headers,
   url: url,
   body: JSON.stringify(body1)
@@ -65,14 +66,11 @@ var headers= {
   if (!error && response.statusCode == 200) {
     console.log(body);
 		console.log("evento aggiunto!")
-   
-    res.send(body);
     }
   else {
     console.log(error);
-		res.send(body);
   }
   });
-	
-});
-app.listen(3000);
+}
+module.exports.aggiungiEvento= aggiungiEvento;
+module.exports.accessoGoogle= accessoGoogle;
