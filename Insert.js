b1cbb595dcfba7fdeb7197897b53661149c3eef0
francescapellecchia <francescapellecchia96@gmail.com>
2019-04-14 19:08:56 +0200

    update

--- Insert.js
+++ Insert.js
@@ -3,80 +3,85 @@ var request = require('request');
 var bodyParser = require("body-parser");
 var app = express();
 var server = require('./app');
-var ws=require('ws');
+var fs=require('fs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/*funzione per reindirizzamento all'accesso a Google*/
 function accessoGoogle(req, res){
   res.redirect("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/calendar&response_type=code&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http%3A%2F%2Flocalhost:8888&client_id=client_id");
-
 };
 
-function controllaEvento(a_t,ws){
-    //var url='https://www.googleapis.com/calendar/v3/calendars/primary/events';
-        var lista2=new Array();
-
+/*funzione per controllare se un evento è presente in una data specifica*/
+function controllaEvento(a_t,req,res){
+    var lista2=new Array();
+		var cantante=server.getCantante();
 		var info= server.getEvento();
 		var d=info.split(" ")[0];
-		console.log(d);
-        if(d.length>11){
+    if(d.length>11){
 			var data=d.split("T")[0];
-			console.log(data);
-            dateagg=data;
+      dateagg=data;
 		}
-        else{
-            dateagg=d;
-        }
-	var options={
-  url:'https://www.googleapis.com/calendar/v3/calendars/primary/events',
-  headers: {
-    'Authorization': 'Bearer '+a_t,
+    else{
+       dateagg=d;
     }
-	};
+		var options={
+  		url:'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+  		headers: {
+    	'Authorization': 'Bearer '+a_t,
+    	}
+		};
     request(options, function callback(error, response, body){
-        if (!error && response.statusCode == 200){
-        var info = JSON.parse(body);
-        var lista=info.items;
-
+    	if (!error && response.statusCode == 200){
+      	var info = JSON.parse(body);
+      	var lista=info.items;
         if(lista!=null){
-
-            var i;
-
-            for(i=0; i<lista.length; i++){
-                if(lista[i].start.date!=undefined){
-                    lista2.push(lista[i].start.date)
-                }
-                else{
-                    var datetime=lista[i].start.dateTime;
-                    var datanormale=datetime.split('T')[0];
-                    lista2.push(datanormale);
-                }
-            }
-
-
-           if(lista2.includes(dateagg)){
-
-                console.log("evento già aggiunto!!");
-                    ws.send("Evento già aggiunto!");
+					var i;
+          for(i=0; i<lista.length; i++){
+          	if(lista[i].start.date!=undefined){
+            	lista2.push(lista[i].start.date)
             }
             else{
-                aggiungiEvento(a_t,ws);
-
+            	var datetime=lista[i].start.dateTime;
+              var datanormale=datetime.split('T')[0];
+              lista2.push(datanormale);
             }
-}
-
-            else{
-                aggiungiEvento(a_t,ws);
-
-            }
-
-
+        	}
+			  	if(lista2.includes(dateagg)){
+		      	console.log("evento già aggiunto!!");
+		        fs.readFile('/home/biar/Desktop/ProgettoRC/canzoni.txt','utf8',function(err1,canzoni){
+						if(err1){
+							console.log(err1);
+						}else{
+							fs.readFile('/home/biar/Desktop/ProgettoRC/estratto.txt','utf8',function(err2,estratto){
+								if(err2){
+									console.log(err2);
+								}else{
+									fs.readFile('/home/biar/Desktop/ProgettoRC/immagini.txt','utf8',function(err3,img){
+										if(err3){
+											console.log(err3);
+										}else{
+											console.log(estratto);
+											res.render('pages/quattro', { t: 'Evento già aggiunto', canzone: canzoni, estratto: estratto, img: img,cantante:cantante});
+										}
+									});	
+								}
+							});
+						}
+					});
+		      }
+		      else{
+		      	aggiungiEvento(a_t,req,res);
+		      }
+				}
+        else{
+          aggiungiEvento(a_t,req,res);
         }
+      }
     });
-
 }
 
-function aggiungiEvento(a_t,ws){
-
+/*aggiunge l'evento al calendario*/
+function aggiungiEvento(a_t,req,res){
   var url= 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
   var info= server.getEvento();
 	var cantante=server.getCantante();
@@ -90,7 +95,6 @@ function aggiungiEvento(a_t,ws){
 		var headers= {
     'Authorization': 'Bearer '+a_t,
 		'Content-Type': 'application/json'
-
     };
 		var body1={
 		'descrition' : 'Evento musicale',
@@ -98,11 +102,11 @@ function aggiungiEvento(a_t,ws){
 		'location': luogo,
 		'end': {
 		'dateTime' : data+'T23:59:00+02:00',
-		'timeZone': 'Europe/'+citta
+		'timeZone': 'Europe/Rome'
 		},
 		'start':{
 		'dateTime': completa2+'+02:00',
-		'timeZone' : 'Europe/'+citta
+		'timeZone' : 'Europe/Rome'
 		}
 		};
 	}else{
@@ -112,17 +116,17 @@ function aggiungiEvento(a_t,ws){
 				'Content-Type': 'application/json'
 
 				};
-			var body1={
-			'descrition' : 'Evento musicale',
-			'summary' : 'Concerto di '+cantante,
-			'location': luogo,
-			'end': {
-			'date' : completa
-			},
-			'start':{
-			'date': completa
-			}
-			};
+		var body1={
+		'descrition' : 'Evento musicale',
+		'summary' : 'Concerto di '+cantante,
+		'location': luogo,
+		'end': {
+		'date' : completa
+		},
+		'start':{
+		'date': completa
+		}
+		};
 	}
 
   request.post({headers: headers,
@@ -130,9 +134,26 @@ function aggiungiEvento(a_t,ws){
   body: JSON.stringify(body1)
   },function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body);
-		console.log("evento aggiunto!");
-		ws.send("Evento aggiunto!")
+    fs.readFile('/home/biar/Desktop/ProgettoRC/canzoni.txt','utf8',function(err1,canzoni){
+					if(err1){
+						console.log(err1);
+					}else{
+						fs.readFile('/home/biar/Desktop/ProgettoRC/estratto.txt','utf8',function(err2,estratto){
+							if(err2){
+								console.log(err2);
+							}else{
+								fs.readFile('/home/biar/Desktop/ProgettoRC/immagini.txt','utf8',function(err3,img){
+								if(err3){
+									console.log(err3);
+								}else{
+									console.log(estratto);
+									res.render('pages/quattro', { t: 'Evento aggiunto', canzone: canzoni, estratto: estratto, img: img,cantante:cantante});
+								}
+							});
+						}
+					});
+				}
+			});
     }
   else {
     console.log(error);
